64a4775437238fee8a61e60ecc70dd99795d08ac
aman <amanagarwalx123@gmail.com>
2023-12-13 12:32:32 +0530

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,28 @@ import Card from "@/components/Card";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="pb-16 ">
-      <nav className="h-24 bg-neutral-950 flex items-center justify-between mx-auto px-20 shadow-md gap-20">
-        <span className="text-3xl font-mono font-bold text-red-400 ">
-          🚀 IdeaHub
-        </span>
-        <span className="space-x-4">
-          <Link href='/pro' className="text-xl font-sans font-medium text-red-200 bg-neutral-600 p-2 px-6 rounded-full hover:opacity-80 transition">Pro 🔥</Link>
-          <button className="text-xl font-sans font-bold text-neutral-800 bg-red-300 p-2 px-6 rounded-full hover:opacity-80 transition">Sign In</button>
-        </span>
-      </nav>
-      <main className="pt-16 max-w-[1500px] flex flex-wrap items-center justify-center gap-16 mx-auto w-fit">
-        <Card path='/book' title='Books Recommender' />
-        <Card path='/gift' title='Gifts Recommender' />
-        <Card path='/meme' title='Memes Generator' />
-        <Card path='/music' title='Music Playlist Recommender' />
-        <Card path='/quote' title='Quotes Generator' />
-        <Card path='/recipe' title='Recipe Recommender' />
-        <Card path='/sign' title='Digital Signer' />
-      </main>
-    </div>
+    // <div className="pb-16 ">
+    //   <nav className="h-24 bg-neutral-950 flex items-center justify-between mx-auto px-20 shadow-md gap-20">
+    //     <span className="text-3xl font-mono font-bold text-red-400 ">
+    //       🚀 IdeaHub
+    //     </span>
+    //     <span className="space-x-4">
+    //       <Link href='/pro' className="text-xl font-sans font-medium text-red-200 bg-neutral-600 p-2 px-6 rounded-full hover:opacity-80 transition">Pro 🔥</Link>
+    //       <button className="text-xl font-sans font-bold text-neutral-800 bg-red-300 p-2 px-6 rounded-full hover:opacity-80 transition">Sign In</button>
+    //     </span>
+    //   </nav>
+    //   <main className="pt-16 max-w-[1500px] flex flex-wrap items-center justify-center gap-16 mx-auto w-fit">
+    //     <Card path='/book' title='Books Recommender' />
+    //     <Card path='/gift' title='Gifts Recommender' />
+    //     <Card path='/meme' title='Memes Generator' />
+    //     <Card path='/music' title='Music Playlist Recommender' />
+    //     <Card path='/quote' title='Quotes Generator' />
+    //     <Card path='/recipe' title='Recipe Recommender' />
+    //     <Card path='/sign' title='Digital Signer' />
+    //   </main>
+    // </div>
+    <div className='flex items-center justify-center text-4xl font-bold mx-auto h-screen' >
+      Under Maintenance, will launch soon.
+    </div >
   )
 }
